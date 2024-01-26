1db4dabe361af181eebd0d2c354bd68dfd1043d6
karinapichardo <pichardokarina21@yahoo.com>
2024-01-25 21:24:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./pichardo.jpeg";
+import youtubeLogo from "./youtube.png";
+import xLogo from "./twitter.png";
+import mediumLogo from "./medium.png";
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="profilePic" alt="logo" />
+        <p className="name">Karina Pichardo</p>
+        <div className="wrapper">
+          <a
+            href="https://www.youtube.com/channel/UCx_X6YawdmcX1ojf8tRClKw"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="mediaContainer">
+              <img
+                src={youtubeLogo}
+                className="youtubeLogo"
+                alt="youtube logo"
+              />
+              <p className="mediaName">Youtube</p>
+            </div>
+          </a>
+
+          <a
+            href="https://twitter.com/karinapichardox"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="mediaContainer">
+              <img src={xLogo} className="twitterLogo" alt="x logo" />
+              <p className="mediaName">Twitter</p>
+            </div>
+          </a>
+
+          <a
+            href="https://karinapichardo.medium.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="mediaContainer">
+              <img
+                src={mediumLogo}
+                className="youtubeLogo"
+                alt="medium logo"
+              />
+              <p className="mediaName">Medium</p>
+            </div>
+          </a>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
